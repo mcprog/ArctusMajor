5fe071326f1d31f25a48c742b27e81990ec942ae
Ky Price <mkyprice@gmail.com>
2019-11-10 19:48:13 -0800

    Adds missed scripts

--- scripts/Coordinate.gd
+++ scripts/Coordinate.gd
@@ -1,15 +1,29 @@
 extends Node2D
 
-func _ready():
-	pass
+var height:float
+var moisture:float
 
-func spawn(pos:Vector2, value:float)->void:
+const GROUND = {
+	'water' : Vector2(-1, 0.25),
+	'radioactive' : Vector2(0.6, 0.8),
+}
+
+const FEATURES = {
+	'ruined city' : Vector2(.1, .13),
+}
+
+func spawn(pos:Vector2, height:float, feature:float)->void:
+#	Set Position
 	position = pos
-	var col := Color.white
-	if (value < 0.25):
-		col = Color(0, 0, value + .75)
-	elif (value < .35):
-		col = Color(value + .65, value + .65, 0)
-	elif (value < .9):
-		col = Color(0, value + .1, 0)
-	$Sprite.modulate = col
+	
+#	Ground
+	if (height <= GROUND['water'].y):
+		$Ground.texture = preload("res://textures/map_textures/water_tile.png")
+	elif (height >= GROUND['radioactive'].x && height <= GROUND['radioactive'].y):
+		$Ground.texture = preload("res://textures/map_textures/radioactive_tile.png")
+	else:
+		$Ground.texture = preload("res://textures/map_textures/desert_tile.png")
+	
+#	Features
+	if (feature > FEATURES['ruined city'].x && feature < FEATURES['ruined city'].y && height > GROUND['water'].y):
+		$Feature.texture = preload("res://textures/map_textures/ruined_city_tile.png")
--- scripts/WorldMap.gd
+++ scripts/WorldMap.gd
@@ -9,25 +9,30 @@ export (float, 0, 20, .01) var lacunarity = 3
 export (float, 0, 1, .01) var persistance = 0.4
 
 const TILE_SIZE = 16
-const MAP_SIZE = 128
+const MAP_SIZE = 64
 
 var coord:PackedScene = preload("res://Scenes/Coordinate.tscn")
-var noise_world:OpenSimplexNoise
+var height_map:OpenSimplexNoise
+var temperature_map:OpenSimplexNoise
+var percipitation_map:OpenSimplexNoise
 
 func _ready()->void:
-	var thread = Thread.new()
 	if (use_random_seed):
 		SEED = randi()
 	z_index = -1
-	noise_world = OpenSimplexNoise.new()
-	noise_world.seed = SEED
-	noise_world.octaves = octaves
-	noise_world.period = period
-	noise_world.lacunarity = lacunarity
-	noise_world.persistence = persistance
-	thread.start(self, "generate_map")
+	height_map = init_noise(SEED, octaves, period, lacunarity, persistance)
+	temperature_map = init_noise(SEED / 13.173213, octaves, period, lacunarity, persistance)
 	generate_map()
-	thread.wait_to_finish()
+
+func init_noise(n_seed:float, n_oct:float, 
+	n_period:float, n_lac:float, n_pers:float)->OpenSimplexNoise:
+	var noise = OpenSimplexNoise.new()
+	noise.seed = n_seed
+	noise.octaves = n_oct
+	noise.period = n_period
+	noise.lacunarity = n_lac
+	noise.persistence = n_pers
+	return noise
 
 func generate_map()->void:
 	var offset := Vector2()
@@ -40,11 +45,11 @@ func generate_map()->void:
 				dist = (MAP_SIZE * .15) / dist
 			else:
 				dist = 2
-			spawn_coord(offset, dist * ((noise_world.get_noise_2dv(offset) + 1) * .5))
+			spawn_coord(offset, dist * ((height_map.get_noise_2dv(offset) + 1) / 2))
 
-func spawn_coord(pos:Vector2, value:float)->void:
+func spawn_coord(pos:Vector2, height:float)->void:
 	var new_coord = coord.instance()
-	new_coord.spawn(pos, value)
+	new_coord.spawn(pos, height, randf())
 	add_child(new_coord)
 
 func _process(delta):	
